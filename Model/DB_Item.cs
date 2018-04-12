b496f84002c0e09e4950d1a8cf1f47cf03a39824
pavel.torres <pavel.torres@mitechnologiesinc.com>
2018-04-11 17:40:19 -0700

    Add proyect

---
+++ Model/DB_Item.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Model
+{
+    public class DB_Item
+    {
+        public List<Item> ListItem()
+        {
+            var items = new List<Item>();
+            try
+            {
+                using (var db = new DBContext())
+                {
+                    items = db.Items.Take(100).OrderByDescending(x => x.sold_quantity).ToList();
+                }
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+            return items;
+        }
+        public Item Obtener(string id)
+        {
+            var items = new Item();
+            try
+            {
+                using (var db = new DBContext())
+                {
+                    items = db.Items
+                            .Where(x => x.ItemId == id)
+                            .First();
+                }
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+            return items;
+        }
+        public void Guardar(Item model)
+        {
+            try
+            {
+                using (var db = new DBContext())
+                {
+                    if (model.ItemId == "")
+                    {
+                        db.Entry(model).State = EntityState.Added;
+                    }
+                    else
+                    {
+                        db.Entry(model).State = EntityState.Modified;
+                    }
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
---
+++ WebMvc/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Model;
+
+namespace WebMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        private DB_Item item = new DB_Item();
+        private Item newitem = new Item();
+
+        public ActionResult Index()
+        {
+            return View(item.ListItem());
+        }
+        public ActionResult Crud(string id = "")
+        {
+            return View(
+                        id != "" ? item.Obtener(id)
+                            : newitem
+                );
+        }
+        public ActionResult Ver(string id)
+        {
+            return View(item.Obtener(id));
+        }
+        public ActionResult Save(Item model)
+        {
+            item.Guardar(model);
+            return Redirect("~/home/crud/" + newitem.ItemId);
+        }
+    }
+}
